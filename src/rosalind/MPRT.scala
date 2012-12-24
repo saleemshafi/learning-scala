6a1fb9c3933bafe627bfb65c6b859634537e3c64
Saleem Shafi <saleemshafi@gmail.com>
2012-12-24 11:02:34 -0600

    fixing the problem with the MPRT solution for cases where the protein matches overlap (regex doesn't find 'em)

--- src/rosalind/MPRT.scala
+++ src/rosalind/MPRT.scala
@@ -5,9 +5,18 @@ import scala.io.Source
 
 object MPRT extends App with RosalindProblem {
 	def findOccurrences(p:Regex)(proteinId:String):Seq[Int] = {
+	  
+	  def findIndexes(indexes: Seq[Int], data:String, p:Regex, offset:Int):Seq[Int] = {
+		 p.findFirstMatchIn(data.substring(offset)) match {
+		   case None => indexes
+		   case Some(m) => findIndexes(indexes:+(offset+m.start+1), data, p, offset+m.start+1)
+		 }
+	  }
+	  
 	  val proteinFasta = Source.fromURL("http://www.uniprot.org/uniprot/"+proteinId+".fasta").mkString
 	  val (pId, data) = GC.parseFASTA(proteinFasta.split('\n').toList).head
-	  (for (m <- p.findAllIn(data).matchData) yield m.start+1).toList
+	  
+	  findIndexes(Nil, data, p, 0)
 	}
   
 }
--- test/rosalind/MPRTTestSuite.scala
+++ test/rosalind/MPRTTestSuite.scala
@@ -5,21 +5,41 @@ import rosalind.MPRT._
 import scala.collection.mutable.Stack
  
 class MPRTTestSuite extends FunSuite {
+  test("finding motifs with overlapping matches") {
+    val proteinIDs = """P02725_GLP_PIG""".split("\r\n").toList
+
+	val pattern = "N[^P][ST][^P]".r //  N{P}[ST]{P}
+	
+	val occurs = findOccurrences(pattern) _
+	expect(List(16, 19, 39)) { occurs("P02725_GLP_PIG").toList }
+  }
+  
   test("finding motifs with data from problem") {
     val proteinIDs = """A2Z669
 B5ZC00
 P20840_SAG1_YEAST""".split("\r\n").toList
 
 	val pattern = "N[^P][ST][^P]".r //  N{P}[ST]{P}
-	expect(Nil) { findOccurrences(pattern)("A2Z669").toList }
-	expect(List(85, 118, 142, 306, 395)) { findOccurrences(pattern)("B5ZC00").toList }
-	expect(List(79, 109, 135, 248, 306, 348, 364, 402, 485, 501, 614)) { findOccurrences(pattern)("P20840_SAG1_YEAST").toList }
+	val occurs = findOccurrences(pattern) _
+	expect(Nil) { occurs("A2Z669").toList }
+	expect(List(85, 118, 142, 306, 395)) { occurs("B5ZC00").toList }
+	expect(List(79, 109, 135, 248, 306, 348, 364, 402, 485, 501, 614)) { occurs("P20840_SAG1_YEAST").toList }
   }
 
   test("finding motifs with real data") {
-    val proteinIDs = """A2Z669
-B5ZC00
-P20840_SAG1_YEAST""".split("\r\n").toList
+    val proteinIDs = """Q9QSP4
+Q8ER84
+P02748_CO9_HUMAN
+Q67JS9
+Q1JHI2
+P01045_KNH2_BOVIN
+P01588_EPO_HUMAN
+P02765_A2HS_HUMAN
+B4U0J5
+P37803
+A8GP89
+P27918_PROP_HUMAN
+P81428_FA10_TROCA""".split("\r\n").toList
 
 	val pattern = "N[^P][ST][^P]".r //  N{P}[ST]{P}
 	
