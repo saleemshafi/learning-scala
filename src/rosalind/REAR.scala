33f43fdaaadd0a04762fe99299ef7283a4b26798
Saleem Shafi <saleemshafi@gmail.com>
2012-12-28 10:43:00 -0600

    finally solved the REAR problem with the help of the included paper

--- src/rosalind/REAR.scala
+++ src/rosalind/REAR.scala
@@ -1,35 +1,73 @@
 package rosalind
 
 import scala._
+import scala.annotation.tailrec
 
 object REAR extends App with RosalindProblem {
-  def stripCommonPrefix[T](a:List[T], b:List[T]):(List[T], List[T]) = (a, b) match {
-    case (Nil, Nil) => (Nil, Nil)
-    case (aFirst::aRest, bFirst::bRest) => 
-	    if (aFirst == bFirst) stripCommonPrefix(aRest, bRest)
-	    else (a, b)
+  def reverse[T](orig:List[T], start:Int, end:Int):List[T] =
+    (orig.view.take(start) ++ orig.view(start, end+1).reverse ++ orig.view(end+1, orig.length)).toList
+    
+  def inversePermutation(orig:List[Int]):List[Int] = 
+    ((orig zipWithIndex) sortBy (_._1)) map (_._2 + 1)
+    
+  def applyPermutation(orig:List[Int], perm:List[Int]) =
+    orig map (i => perm(i-1))
+  
+  type Permutation = List[Int]
+  type Breakpoint = (Int, Int)
+  
+  def breakpoints(p:Permutation):List[Breakpoint] = 
+    (for (pair <- (( 0::(p:+(p.length+1)) zipWithIndex) sliding 2); if (Math.abs(pair(0)._1 - pair(1)._1) != 1)) 
+    	yield (pair(0)._2, pair(1)._2)).toList
+  
+  def breakpointCombos(bps:List[Breakpoint]):List[(Int, Int)] = bps match {
+    case Nil => Nil
+    case _ =>
+	    (for (first <- (bps.init map (_._2)); second <- (bps.tail map (_._1)); if (first < second))
+	    	yield (first, second)).toList
+  }
+    
+  def anyStrip(first:Int, last:Int) = true
+  def increasingStrip(first:Int, last:Int) = first <= last
+  def decreasingStrip(first:Int, last:Int) = first > last
+  
+  def strips(p:List[Int]):List[List[Int]] = strips(p, anyStrip)
+  def strips(p:List[Int], filter:(Int, Int) => Boolean):List[List[Int]] =
+    (for (pair <- (breakpoints(p) sliding 2); if (pair(0)._2 != pair(1)._1))
+      yield (pair(0)._2 to pair(1)._1).toList).toList
+
+  def isIdentity(pi:Permutation) = {
+    val pwi = pi zipWithIndex
+    val l = (for ((v, i) <- pwi; if (v != i+1)) yield v)
+    l.isEmpty
   }
 
-  def reverse[T](orig:List[T], elem: T):List[T] = {
-    val (beginning, end) = orig.splitAt(orig.indexOf(elem)+1)
-    beginning.reverse ++ end
-  } 
+  def numReversalsForSort(orig:Permutation):Int = orig match {
+	    case Nil => 0
+	    case _ => if (orig.last == orig.length) numReversalsForSort(orig.init) 
+	    		  else {
+	    		    val i = orig.indexOf(orig.length)
+	    		    1 + numReversalsForSort(reverse(orig, i, orig.length-1))
+	    		  }
+    }
 
-  def getNumReversals[T](start:List[T], end:List[T]):Int =
-    getNumReversals(start, end, 0)
+  def getNumReversals(sigma:Permutation, tau:Permutation):Int = {
     
-  def getNumReversals[T](start:List[T], end:List[T], permsSoFar:Int):Int = 
-    stripCommonPrefix(start, end) match {
-    	case (Nil, Nil) => permsSoFar
-    	case (source, target) => {
-    	  Math.min(
-    			  getNumReversals(reverse(source, target.head), target, permsSoFar+1),
-    			  getNumReversals(reverse(source.reverse, target.last), target.reverse, permsSoFar+1))    			  
-    	}
-  	} 
-  
-  val data = getInput("REAR.input").grouped(3)
-  val answers = for (List(start, end, space) <- data)
-    yield getNumReversals(start.split(" ").toList, end.split(" ").toList, 0) 
-  println(answers.mkString(" "))
+    @tailrec
+    def getNumReversals(revCount:Int, perms:List[(Permutation, List[Breakpoint])]):Int = perms match {
+      case Nil => -1
+      case (perm, Nil)::anything => revCount
+      case _ => {
+      	val allRevsPlusBPs = (for ((pi, bps) <- perms; (start, end) <- breakpointCombos(bps); r = reverse(pi, start-1, end-1)) 
+      	  yield (r, breakpoints(r)))
+      	val minLength = (allRevsPlusBPs minBy (a => a._2.length))._2.length
+        val candidates = (for (rbp <- allRevsPlusBPs; if (rbp._2.length == minLength) )
+          yield (rbp._1, rbp._2))
+        getNumReversals(revCount + 1, candidates.removeDuplicates)
+      }
+    }
+    
+    val pi = applyPermutation(sigma, inversePermutation(tau))
+    getNumReversals(0, List((pi, breakpoints(pi))))
+  }
 }
--- test/rosalind/REARTestSuite.scala
+++ test/rosalind/REARTestSuite.scala
@@ -5,36 +5,86 @@ import rosalind.REAR._
 import scala.collection.mutable.Stack
  
 class REARTestSuite extends FunSuite {
-  def getDataset(data:String) = 
-    (data.split(" ") map (a => a.toInt)).toList
   
-  test("reversal distance with data (set #1) from problem") {
-    val start = getDataset("1 2 3 4 5 6 7 8 9 10")
-    val end = getDataset("3 1 5 2 7 4 9 6 10 8")
-	expect(9) { getNumReversals(start, end) }
+  def IntList(data:String) = data.split(" ").map(Integer.parseInt(_)).toList
+    
+  test("inverse permutation") {
+	  expect(IntList("3 1 2 5 4")) { inversePermutation(IntList("2 3 1 5 4")) }
+  }
+  
+  test("apply permutation") {
+	  expect(IntList("4 1 5 3 2")) { applyPermutation(IntList("5 2 4 1 3"), IntList("3 1 2 5 4")) }
+  }
+  
+  test("get breakpoints") {
+    expect(List()) { breakpoints(IntList("1 2 3 4 5")) }
+    expect(List((0,1), (2,3), (3,4), (6,7))) { breakpoints(IntList("2 3 1 6 5 4")) }
+  }
+    
+  test("get breakpoint combos") {
+    val bps = breakpoints(IntList("2 3 1 6 5 4"))
+    expect(List((1,2),(1,3),(1,6),(3,6),(4,6))) { breakpointCombos(bps) }
+  }
+  
+  test("test for identity both positive and negative") {
+    expect(false) { isIdentity(IntList("5 2 3 1 4")) }
+    expect(false) { isIdentity(IntList("5 4 3 2 1")) }
+    expect(true) { isIdentity(IntList("1 2 3 4 5")) }
+  }
+  
+  test("reversal distance with shortened data") {
+    val start = IntList("1 2 3 4 5")
+    val end = IntList("3 1 5 2 4")
+	expect(4) { getNumReversals(start, end) }
   }
 
+  test("reversal distance with data (set #1) from problem") {
+    val start = IntList("1 2 3 4 5 6 7 8 9 10")
+    val end = IntList("3 1 5 2 7 4 9 6 10 8")
+	expect(9) { getNumReversals(start, end) }
+  }
   test("reversal distance with data (set #2) from problem") {
-    val start = getDataset("3 10 8 2 5 4 7 1 6 9")
-    val end = getDataset("5 2 3 1 7 4 10 8 6 9")
+    val start = IntList("3 10 8 2 5 4 7 1 6 9")
+    val end = IntList("5 2 3 1 7 4 10 8 6 9")
 	expect(4) { getNumReversals(start, end) }
   }
 
   test("reversal distance with data (set #3) from problem") {
-    val start = getDataset("8 6 7 9 4 1 3 10 2 5")
-    val end = getDataset("8 2 7 6 9 1 5 3 10 4")
+    val start = IntList("8 6 7 9 4 1 3 10 2 5")
+    val end = IntList("8 2 7 6 9 1 5 3 10 4")
 	expect(5) { getNumReversals(start, end) }
   }
 
   test("reversal distance with data (set #4) from problem") {
-    val start = getDataset("3 9 10 4 1 8 6 7 5 2")
-    val end = getDataset("2 9 8 5 1 7 3 4 6 10")
+    val start = IntList("3 9 10 4 1 8 6 7 5 2")
+    val end = IntList("2 9 8 5 1 7 3 4 6 10")
 	expect(7) { getNumReversals(start, end) }
   }
 
   test("reversal distance with data (set #5) from problem") {
-    val start = getDataset("1 2 3 4 5 6 7 8 9 10")
-    val end = getDataset("1 2 3 4 5 6 7 8 9 10")
+    val start = IntList("1 2 3 4 5 6 7 8 9 10")
+    val end = IntList("1 2 3 4 5 6 7 8 9 10")
 	expect(0) { getNumReversals(start, end) }
+  }
+
+  test("reversal distances with real data") {
+	  val data = """7 8 5 6 2 10 9 4 1 3
+4 8 9 3 6 7 10 1 5 2
+
+1 3 8 10 4 9 6 5 2 7
+6 3 9 1 10 2 4 7 8 5
+
+3 4 9 8 1 7 10 2 5 6
+7 6 2 9 5 3 8 4 10 1
+
+5 9 3 8 2 4 1 10 6 7
+6 8 9 5 7 2 1 4 3 10
+
+3 2 5 4 9 1 6 8 7 10
+5 3 9 2 6 4 7 1 10 8""".split("\r\n").toList
+    
+	  (data grouped 3) foreach {
+	    case first::second::whatever => print( getNumReversals(IntList(first), IntList(second)) + " " )
+	  }
   }
 }
