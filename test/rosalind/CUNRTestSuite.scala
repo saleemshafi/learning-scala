97c67123c97c9cf1acc8297bafcf03e7794d3c75
Saleem Shafi <saleemshafi@gmail.com>
2012-12-28 23:17:16 -0600

    adding logic to CUNRTestSuite to solve the ROOT problem

--- test/rosalind/CUNRTestSuite.scala
+++ test/rosalind/CUNRTestSuite.scala
@@ -10,24 +10,33 @@ class CUNRTestSuite extends FunSuite with RosalindProblem {
   
   
   // http://en.wikipedia.org/wiki/Unrooted_binary_tree#Enumeration
-  def getNumTreesWithXNodes(n:Int):Int = {
+  def getNumUBTreesWithXNodes(n:Int):Int = {
     val s = n-2
     val e = (2*n)-4
     val numerator = (s+1 to e).foldLeft(BigInt.apply(1))( (a:BigInt, b:Int) => (a * b) )
     val denominator = (1 to n-2).foldLeft(BigInt.apply(1))( (a:BigInt, b:Int) => (a * 2) )
     ((numerator / denominator) % 1000000).intValue
   }
+
+  def getNumRBTreesWithXNodes(n:Int):Int = {
+    (1 to (2*n - 2)).foldLeft(1)((a:Int, b:Int) => if (b % 2 == 0) a else (a *b) % 1000000 )
+  }
   
-  test("tree count") {
+  test("unrooted binary tree count") {
 //    expect(1) { getNumTreesWithXNodes(2) }
-    expect(1) { getNumTreesWithXNodes(3) }
-    expect(3) { getNumTreesWithXNodes(4) }
-    expect(15) { getNumTreesWithXNodes(5) }
-    expect(105) { getNumTreesWithXNodes(6) }
-    expect(945) { getNumTreesWithXNodes(7) }
-    expect(10395) { getNumTreesWithXNodes(8) }
-    expect(135135) { getNumTreesWithXNodes(9) }
-    expect(27025) { getNumTreesWithXNodes(10) }
-    expect(609375) { getNumTreesWithXNodes(953) }
+    expect(1) { getNumUBTreesWithXNodes(3) }
+    expect(3) { getNumUBTreesWithXNodes(4) }
+    expect(15) { getNumUBTreesWithXNodes(5) }
+    expect(105) { getNumUBTreesWithXNodes(6) }
+    expect(945) { getNumUBTreesWithXNodes(7) }
+    expect(10395) { getNumUBTreesWithXNodes(8) }
+    expect(135135) { getNumUBTreesWithXNodes(9) }
+    expect(27025) { getNumUBTreesWithXNodes(10) }
+    expect(609375) { getNumUBTreesWithXNodes(953) }
+  }
+  
+  test("rooted binary tree count") {
+    expect(15) { getNumRBTreesWithXNodes(4) }
+    expect(859375) { getNumRBTreesWithXNodes(972) }
   }
 }
